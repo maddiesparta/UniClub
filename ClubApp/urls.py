85ae7274d1ffdc4a67d1886dbf0f37c83eaf69d6
Maddi Esparta <maddi.esparta@opendeusto.es>
2025-11-04 13:53:46 +0100

    Add actividades and organizadores views

--- ClubApp/urls.py
+++ ClubApp/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('eventos/', views.eventos, name='eventos'),
     path('eventos/<int:evento_id>/', views.detalle_evento, name='detalle_evento'),
+    path('organizadores/', views.organizadores, name='organizadores'),
+    path('organizadores/<int:organizador_id>/', views.detalle_organizador, name='detalle_organizador'),
+    path('actividades/', views.actividades, name='actividades'),
+    path('actividades/<int:actividad_id>/', views.detalle_actividad, name='detalle_actividad'),
 ]
--- ClubApp/views.py
+++ ClubApp/views.py
@@ -11,8 +11,31 @@ def eventos(request):
     context = {'eventos': eventos}
     return render(request, 'eventos.html', context)
 
+def actividades(request):
+    actividades=Actividad.objects.all()
+    context={'actividades': actividades}
+    return render(request, 'actividades.html', context)
+
 def detalle_evento(request, evento_id):
     actividades = Actividad.objects.all()
     context = {'actividades': actividades,
                'evento_id': evento_id}
     return render(request, 'lista_actividades.html', context)
+
+def organizadores(request):
+    organizadores = Organizador.objects.all()
+    context= {'organizadores': organizadores}
+    return render(request,'organizadores.html', context)
+
+def detalle_organizador(request, organizador_id):
+    eventos = Evento.objects.all()
+    context={'eventos':eventos,
+             'organizador': get_object_or_404(Organizador, id=organizador_id)} 
+    return render(request, 'lista_organizadores.html', context)
+
+def detalle_actividad(request, actividad_id):
+    actividad = get_object_or_404(Actividad, id = actividad_id)
+    context = {'actividad' : actividad}
+    return render(request,'detalle_actividad.html',context)
+    
+
