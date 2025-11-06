e1731c18421f3b16b73014a8ff90c05f6ce80a63
Markel Mencía <markel.mnc@gmail.com>
2025-11-06 09:10:44 +0100

    Rearrange template file structure

--- ClubApp/urls.py
+++ ClubApp/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('eventos/', views.eventos, name='eventos'),
-    path('eventos/<int:evento_id>/', views.detalle_evento, name='detalle_evento'),
+    path('eventos/<int:evento_id>/', views.evento_actividades, name='evento_actividades'),
     path('organizadores/', views.organizadores, name='organizadores'),
-    path('organizadores/<int:organizador_id>/', views.detalle_organizador, name='detalle_organizador'),
+    path('organizadores/<int:organizador_id>/', views.organizador_eventos, name='organizador_eventos'),
     path('actividades/', views.actividades, name='actividades'),
     path('actividades/<int:actividad_id>/', views.detalle_actividad, name='detalle_actividad'),
 ]
--- ClubApp/views.py
+++ ClubApp/views.py
@@ -9,32 +9,31 @@ def index(request):
 def eventos(request):
     eventos = Evento.objects.all()
     context = {'eventos': eventos}
-    return render(request, 'eventos.html', context)
+    return render(request, 'eventos/eventos.html', context)
 
 def actividades(request):
     actividades=Actividad.objects.all()
     context={'actividades': actividades}
-    return render(request, 'actividades.html', context)
+    return render(request, 'actividades/actividades.html', context)
 
-def detalle_evento(request, evento_id):
+def evento_actividades(request, evento_id):
     actividades = Actividad.objects.all()
     context = {'actividades': actividades, 'evento': get_object_or_404(Evento, id=evento_id)}
-    return render(request, 'lista_actividades.html', context)
+    return render(request, 'eventos/evento_actividades.html', context)
 
 def organizadores(request):
     organizadores = Organizador.objects.all()
     context= {'organizadores': organizadores}
-    return render(request,'organizadores.html', context)
+    return render(request,'organizadores/organizadores.html', context)
 
-def detalle_organizador(request, organizador_id):
+def organizador_eventos(request, organizador_id):
     eventos = Evento.objects.all()
-    context={'eventos':eventos,
-             'organizador': get_object_or_404(Organizador, id=organizador_id)} 
-    return render(request, 'lista_organizadores.html', context)
+    context={'eventos':eventos, 'organizador': get_object_or_404(Organizador, id=organizador_id)} 
+    return render(request, 'organizadores/organizador_eventos.html', context)
 
 def detalle_actividad(request, actividad_id):
     actividad = get_object_or_404(Actividad, id = actividad_id)
     context = {'actividad' : actividad}
-    return render(request,'detalle_actividad.html',context)
+    return render(request,'actividades/detalle_actividad.html',context)
     
 
