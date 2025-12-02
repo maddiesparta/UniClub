e78ddacc39bc65a9f66eab2b402a777763c6d571
Markel Mencía <markel.mnc@gmail.com>
2025-12-02 13:39:46 +0100

    Change views to class-based

--- ClubApp/urls.py
+++ ClubApp/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from . import views
+from django.views.generic import ListView
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('eventos/', views.eventos, name='eventos'),
-    path('eventos/<int:evento_id>/', views.evento_actividades, name='evento_actividades'),
-    path('organizadores/', views.organizadores, name='organizadores'),
-    path('organizadores/<int:organizador_id>/', views.organizador_eventos, name='organizador_eventos'),
-    path('actividades/', views.actividades, name='actividades'),
-    path('actividades/<int:actividad_id>/', views.detalle_actividad, name='detalle_actividad'),
+    path('eventos/', views.EventosListView.as_view(), name='eventos'),
+    path('eventos/<int:evento_id>/', views.ActividadesEventoListView.as_view(), name='evento_actividades'),
+    path('organizadores/', views.OrganizadoresListView.as_view(), name='organizadores'),
+    path('organizadores/<int:organizador_id>/', views.EventosOrganizadorListView.as_view(), name='organizador_eventos'),
+    path('actividades/', views.ActividadesListView.as_view(), name='actividades'),
+    path('actividades/<int:pk>/', views.ActividadDetailView.as_view(), name='detalle_actividad'),
     path('contacto/', views.contacto, name='contacto'),
 ]
--- ClubApp/views.py
+++ ClubApp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Evento, Actividad, Organizador
 from datetime import date, datetime
+from django.views.generic import ListView
+from django.views.generic import DetailView
 
 # Create your views here.
 
@@ -17,35 +19,50 @@ def index(request):
 def obtener_fecha(evento):
     return datetime.strptime(evento.fecha, "%d/%m/%Y").date()
 
-def eventos(request): 
-    eventos = Evento.objects.all()
-    context = {'eventos': eventos}
-    return render(request, 'eventos/eventos.html', context)
+class EventosListView(ListView):
+    model = Evento
+    template_name = "eventos/eventos.html"
+    context_object_name = "eventos"
 
-def actividades(request):
-    actividades=Actividad.objects.all()
-    context={'actividades': actividades}
-    return render(request, 'actividades/actividades.html', context)
+class ActividadesListView(ListView):
+    model = Actividad
+    template_name = "actividades/actividades.html"
+    context_object_name = "actividades"
 
-def evento_actividades(request, evento_id):
-    actividades = Actividad.objects.all()
-    context = {'actividades': actividades, 'evento': get_object_or_404(Evento, id=evento_id)}
-    return render(request, 'eventos/evento_actividades.html', context)
+class ActividadesEventoListView(ListView):
+        model = Actividad
+        context_object_name = "actividades"
+        template_name = "eventos/evento_actividades.html"
 
-def organizadores(request):
-    organizadores = Organizador.objects.all()
-    context= {'organizadores': organizadores}
-    return render(request,'organizadores/organizadores.html', context)
+        def get_context_data(self, **kwargs):
+            # Cargar el contexto base
+            context = super().get_context_data(**kwargs)
+            evento_id = self.kwargs['evento_id']
+            # Añadir un listado de departamentos
+            context['evento'] = get_object_or_404(Organizador, id=evento_id)
+            return context
 
-def organizador_eventos(request, organizador_id):
-    eventos = Evento.objects.all()
-    context={'eventos':eventos, 'organizador': get_object_or_404(Organizador, id=organizador_id)} 
-    return render(request, 'organizadores/organizador_eventos.html', context)
+class OrganizadoresListView(ListView):
+    model = Organizador
+    template_name = "organizadores/organizadores.html"
+    context_object_name = "organizadores"
 
-def detalle_actividad(request, actividad_id):
-    actividad = get_object_or_404(Actividad, id = actividad_id)
-    context = {'actividad' : actividad}
-    return render(request,'actividades/detalle_actividad.html',context)
+class EventosOrganizadorListView(ListView):
+        model = Evento
+        context_object_name = "eventos"
+        template_name = "organizadores/organizador_eventos.html"
+
+        def get_context_data(self, **kwargs):
+            # Cargar el contexto base
+            context = super().get_context_data(**kwargs)
+            organizador_id = self.kwargs['organizador_id']
+            # Añadir un listado de departamentos
+            context['organizador'] = get_object_or_404(Organizador, id=organizador_id)
+            return context
+
+class ActividadDetailView(DetailView):
+    model = Actividad
+    template_name = "actividades/detalle_actividad.html"
     
 def contacto(request):
     return render(request, 'contacto/contacto.html')
