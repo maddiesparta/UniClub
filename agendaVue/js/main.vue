5a0d99ffdcca7ae21e2dda866e455578c99fe262
Markel Mencía <markel.mnc@gmail.com>
2025-12-03 16:07:08 +0100

    Create Vue Contactos project

---
+++ agendaVue/js/main.vue
@@ -0,0 +1,34 @@
+var contactListInstance = new Vue({
+    el: "#contactos",
+    data: {
+        test: "hola",
+        contactos: [
+        {nombre: "Markel", email: "markel@markel.markel", tlf: 611462401},
+        {nombre: "Naia", email: "naia@naia.naia", tlf: 43848343},
+        {nombre: "Maddi", email: "maddi@maddi.maddi", tlf: 3828323},
+        {nombre: "Sergio", email: "sergio@sergio.sergio", tlf: 138238}, 
+        {nombre: "Josu", email: "josu@josu.josu", tlf: 32332323}, 
+        ]
+    },
+    methods: {
+       eliminarContacto: function(index, event) {
+            this.contactos.splice(index, 1);
+        }         
+    }
+});
+
+var createContactInstance = new Vue({
+    el: "#crear-contacto",
+    data: {
+        nombre: '',
+        email: '',
+        tlf: '',
+        test: 'adios'
+    },
+    methods: {
+        crearContacto: function(event) {
+            this.test = "Holaa!"
+            contactListInstance.contactos.push({"nombre": this.nombre, "email": this.email, "tlf": this.tlf})
+        }
+    }
+});
